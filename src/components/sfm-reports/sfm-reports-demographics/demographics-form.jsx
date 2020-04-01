fed680cbc32f040448319a0abcedd874662c7e38
Chirag Jha <chjha@deloitte.com>
2020-04-01 20:18:58 +0530

    CSS changes

--- src/components/sfm-reports/sfm-reports-demographics/demographics-form.jsx
+++ src/components/sfm-reports/sfm-reports-demographics/demographics-form.jsx
@@ -20,18 +20,15 @@ export default function DemographicsForm(props){
     return(
         <div className="demographics-form">
        
-            <LabelledInputField placeholder={false} labelName="Location" data="Bristol"/>
-            <LabelledInputField placeholder={false} labelName="Primary POC" data="Jim Hughes"/>
-            <LabelledInputField placeholder={false} labelName="Primary POC Role" data="Sr. Manager"/>
-        {/* <div className="bottom-border"></div>
-        <div className="bottom-border"></div>
-        <div className="bottom-border"></div> */}
+        <LabelledInputField placeholder={false} labelName="Location" data="Bristol"/>
+        <LabelledInputField placeholder={false} labelName="Primary POC" data="Jim Hughes"/>
+        <LabelledInputField placeholder={false} labelName="Primary POC Role" data="Sr. Manager"/>
         <DropDownMenu data={data[0]} />
         <DropDownMenu data={data[1]} />
         <LabelledInputField placeholder={false} labelName="# of shifts" data="3"/>
-        <DropDownMenu data={data[2]} />
+        <LabelledInputField placeholder={false} labelName="# Employees" data="100-200"/>
         <LabelledInputField placeholder={false} labelName="# of production assets" data="7"/>
-        <LabelledInputField placeholder={false}labelName="site revenue" data="$430,000"/>
+        <LabelledInputField placeholder={false} labelName="site revenue" data="$430,000"/>
         <LabelledInputField placeholder={true} labelName="Site EPITDA (optional)" />
         <LabelledInputField placeholder={true} labelName="OTIF % (optional)" />
         <LabelledInputField placeholder={true} labelName="Site OEE (optional)" />
