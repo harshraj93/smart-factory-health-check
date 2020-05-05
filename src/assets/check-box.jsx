d2921b9b151b5b14db3e6ee80604181ebe307a4f
Chirag Jha <chjha@deloitte.com>
2020-05-05 15:53:27 +0530

    bug fixes

--- src/assets/check-box.jsx
+++ src/assets/check-box.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function CustomCheckBox(props){
-    
+    return(
+    <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id={"customCheck"+props.name} name={props.name} onClick={props.onClick}/>
+            <label class="custom-control-label" for={"customCheck"+props.name}>{props.label}</label>
+    </div>
+    )
 }
