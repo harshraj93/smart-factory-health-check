82149ecbca54dc8e95716bf9a6b78741e4df67fd
Chirag Jha <chjha@deloitte.com>
2020-05-05 18:09:36 +0530

    add new site

--- src/components/sfm-add-new-site/add-new-site.jsx
+++ src/components/sfm-add-new-site/add-new-site.jsx
@@ -4,6 +4,8 @@ import leftIcon from '../../images/icon-small-chevron-left.svg';
 import {withRouter} from 'react-router-dom';
 import CustomCheckBox from '../../assets/check-box';
 import LabelledInputField from '../../assets/input-field';
+import DropDownMenu from '../../assets/drop-down-input-box';
+
 let sectors = ["Consumer Products","Automotive"]
 
 
@@ -24,14 +26,29 @@ function Header(props){
         super(props);
         this.state={
             addNewSector:false,
+            enableButton:false,
+            addSectorDiv:""
         }
         this.props.disableMenu(false)
     }
 
-    handleChange = (e)=>{
-        let name = e.target.name;
-        console.log(name,e)
+    
+    handleChange = async (e)=>{
+            let name = e.currentTarget.getAttribute("name");
+            e.target.value?await this.setState({
+                [name]:e.target.value
+            }):
+            await this.setState({
+                [name]:e.target.getAttribute("value")
+            })
+            console.log(name,this.state[name])
+            
+    }
 
+    showNewSector = ()=>{
+        this.setState({
+            addSectorDiv:!this.state.addSectorDiv
+        })
     }
 
     changeButtonState = ()=>{
@@ -41,9 +58,10 @@ function Header(props){
     }
 
     selectCheckBox = (e)=>{
-        console.log(e.target.name)
+        !this.state[e.target.name+"showInput"]?document.getElementById(e.target.name).style.color="#ffffff":document.getElementById(e.target.name).style.color="#9e9e9e";
         this.setState({
-            [e.target.name+"showInput"]:!this.state[e.target.name+"showInput"]
+            [e.target.name+"showInput"]:!this.state[e.target.name+"showInput"],
+            addNewSector:!this.state.addNewSector
         })
     }
 
@@ -59,8 +77,9 @@ function Header(props){
                     <CustomCheckBox label={element} name={element.replace(/\s/g,'')} onClick={this.selectCheckBox}/>
                     <div className={"show-input-box "+this.state[element.replace(/\s/g,'')+"showInput"]}>
                     <LabelledInputField placeholder={true} 
+                    type="number"
                     changeButtonState={this.changeButtonState} 
-                    labelName="#Number of sites to assess" required={true} 
+                    labelName="# of sites to assess*" required={true} 
                         name="siteNum" onChange={this.handleChange} />
                         </div>
                     </>
@@ -68,12 +87,21 @@ function Header(props){
             })}
            
             </div>
-            <button type="button" className={"add-new-sector "+this.state.addNewSector} >
+            <button type="button" className={"add-new-sector "+this.state.addNewSector} onClick={this.showNewSector}>
                 <span>&#8853;</span> 
                 Add New Sector
             </button>
             
-            
+            <div className="border-bottom" />
+            {this.state.addSectorDiv&&<div className="add-new-sector-text">Add New Sector</div>}
+            {this.state.addSectorDiv&&<div className="container-add-sector">
+            <DropDownMenu placeholder = "Select Sector*" data={["A","B"]} onChange={this.handleChange} dropdownIndex={0}/>
+            <LabelledInputField placeholder={true} 
+                    type="number"
+                    changeButtonState={this.changeButtonState} 
+                    labelName="# of sites to assess*" required={true} 
+                        name="siteNum" onChange={this.handleChange} />
+            </div>}
             <FormNavigationButton buttonStatus={this.state.enableButton} labelName="Next Step" />
             </div>
         )
