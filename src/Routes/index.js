5e65c6d486252cf748361aa65bd39dab198b5e17
tk7-d <62370442+tk7-d@users.noreply.github.com>
2020-05-29 14:16:29 +0530

    loading page

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -20,23 +20,26 @@ export default function Routes(props){
             </Switch>
         )
     }
-    else if(profile!=="Client" && profile!=="Sector"){
+    else if(profile!=="Client" && profile!=="Sector" && profile!==undefined){
         localStorage.setItem("clientview","false");
-    return(
-    <Switch>
-        <Route exact path="/"> <Assessments disableMenu={props.disableMenu}/> </Route>
-        <Route exact path="/analytics"> <Analytics/> </Route>
-        <Route exact path="/utilization"> <UtilSum/> </Route>
-        <Route exact path="/overview"></Route>
-        <Route exact path='/reports'><Reports disableMenu={props.disableMenu} profile={props.userProfile} username={props.username} userEmail={props.userEmail}/></Route>
-        <Route exact path="/addnewclient"><AddNewClient disableMenu={props.disableMenu}/></Route>
-        <Route exact path="/addsitedetails"><AddSiteDetails disableMenu={props.disableMenu}/></Route>
-        <Route exact path="/addbusinessfunctions"><AddBusinessFunctions disableMenu={props.disableMenu}/></Route>
-        <Route exact path="/questionnaire"><QuestionnairePage disableMenu={props.disableMenu} userEmail={props.userEmail}/></Route>
-        <Route exact path="/addnewsite"><AddNewSite disableMenu={props.disableMenu} /></Route>
-    </Switch>
-    );
-}
-
+        return(
+            <Switch>
+                <Route exact path="/"> <Assessments disableMenu={props.disableMenu}/> </Route>
+                <Route exact path="/analytics"> <Analytics/> </Route>
+                <Route exact path="/utilization"> <UtilSum/> </Route>
+                <Route exact path="/overview"></Route>
+                <Route exact path='/reports'><Reports disableMenu={props.disableMenu} profile={props.userProfile} username={props.username} userEmail={props.userEmail}/></Route>
+                <Route exact path="/addnewclient"><AddNewClient disableMenu={props.disableMenu}/></Route>
+                <Route exact path="/addsitedetails"><AddSiteDetails disableMenu={props.disableMenu}/></Route>
+                <Route exact path="/addbusinessfunctions"><AddBusinessFunctions disableMenu={props.disableMenu}/></Route>
+                <Route exact path="/questionnaire"><QuestionnairePage disableMenu={props.disableMenu} userEmail={props.userEmail}/></Route>
+                <Route exact path="/addnewsite"><AddNewSite disableMenu={props.disableMenu} /></Route>
+            </Switch>
+        );
+    }
+    else {
+        this.props.disableMenu(false);
+        return("");
+    }
 
 }
